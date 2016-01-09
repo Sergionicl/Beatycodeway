7a7d4cc88a8897933463f76d033e9b2bd4ddf2c2
Larkov Sergey <lavrinzhuk@mail.ru>
2016-01-09 22:04:04 +0300

    Print_r вывод удобочитаемой информации о переменной

--- www/index.php
+++ www/index.php
@@ -1,6 +1,22 @@
-<?php
-	$title = "Home page";
-	include_once "Header.php"; //Include_once продолжает выполнять действия если даже не нашел файл и выводит все один раз
-	echo "Body of the document";
-	include_once "footer.php";
-?>
+<html>
+<head>
+	<title> Form</title>
+
+
+</head>
+
+<body>
+	<form  name="test" action = "check.php" method = "post">
+		 <label> Name: </label> <br/>
+		 <input type = "text" name = "name" placeholder="Name" />
+		 <label> Email: </label> <br/>
+		 <input type = "text" name = "email" placeholder="mih.lars@mail.ru" />
+		 <label> Massage: </label> <br/>
+		 <textarea name = "message" cols="40" rows="5"></textarea>
+		 <br/>
+		 <input type="submit" name="done" value="Ready"/>
+	</form>
+	
+
+</body>
+</html>
