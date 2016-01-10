e5a08307c8b3cc35704cccdc02dd948a0cff9cdb
Larkov Sergey <lavrinzhuk@mail.ru>
2016-01-10 12:41:45 +0300

    Form with two pages index.php and check.php

--- www/index.php
+++ www/index.php
@@ -8,11 +8,11 @@
 <body>
 	<form  name="test" action = "check.php" method = "post">
 		 <label> Name: </label> <br/>
-		 <input type = "text" name = "name" placeholder="Name" />
+		 <input type = "text" name = "name" placeholder="Name" /><br/>
 		 <label> Email: </label> <br/>
-		 <input type = "text" name = "email" placeholder="mih.lars@mail.ru" />
+		 <input type = "text" name = "email" placeholder="mih.lars@mail.ru" /><br/>
 		 <label> Massage: </label> <br/>
-		 <textarea name = "message" cols="40" rows="5"></textarea>
+		 <textarea name = "message" cols="40" rows="5"></textarea><br/>
 		 <br/>
 		 <input type="submit" name="done" value="Ready"/>
 	</form>
