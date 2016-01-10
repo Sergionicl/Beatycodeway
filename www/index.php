2ef06381faabf0a6647454e60ce23cfd3f661fdc
Larkov Sergey <lavrinzhuk@mail.ru>
2016-01-10 12:55:55 +0300

    Form with one page. Checking form is in the same page

--- www/index.php
+++ www/index.php
@@ -1,12 +1,23 @@
+<?php
+	
+	$name = $_POST["name"];
+	$done = $_POST ["done"];
+	
+	if (isset($done)){//Спрашивает сработала ли кнопка Submit
+		if ($name == "")
+			echo "Please insert anything <a href = '/'> Redo</a>";
+		else
+			header ("Location: index.php");//Перенаправляет на страницы формы. В данном случае не нужна
+	}
+?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title> Form</title>
-
-
 </head>
 
 <body>
-	<form  name="test" action = "check.php" method = "post">
+	<form  name="test" action = "" method = "post">
 		 <label> Name: </label> <br/>
 		 <input type = "text" name = "name" placeholder="Name" /><br/>
 		 <label> Email: </label> <br/>
