654f30beb2d1262d33a85047bf4aad2635749673
Larkov Sergey <lavrinzhuk@mail.ru>
2016-01-16 03:44:53 +0300

    String function

--- www/index.php
+++ www/index.php
@@ -1,14 +1,24 @@
 <?php
-   //Константы
-	echo M_RI."<br/>".M_E;
-	$x = -15;
-	echo abs ($x)."<br/>";//Модуль
-	$y = 49.505345;
-	echo round ($y, 4); //Округление
-	echo ceil ($y); // Округляет к большему. Используется в пейджере
-	echo floor ($y); //Округляет к меньшему
+   //Строковые переменные
+   $string = "This is example";
+   
+   echo strlen ($string)."<br/>";
+   echo strpos ($string, "exa")."<br/>"; //Указать позицию данного выражения в строке
+   echo strpos ($string, "is", 4)."<br/>";
+   
+   if (strpos ($string, "T") === false) echo "T is in"; //Необходимо использовать оператор эквивалентности
+   else echo "T isn't";
+   
+   echo "<br/>".substr($string, 3, 7); //Удалил до 3 символа и после 7. Если поставит -2 - то удалит с конца два элемента. Функция гораздо больше 
+   echo str_replace("is", "abc", $string);//Заменяет. Можно массивами str_replace (array("is",))
+   
+   $str = "<b>XXAA, main font<b/>";
+   
+   echo "<br/>". htmlspecialchars ($str);//Добавление спец символов безопасных подменяющих HTML
+   
+   echo "<br/>".strtolower ($string)."<br/>".strtoupper($string);//Верхний и нижний регистры
 	
-	echo "<br/>".mt_rand(1, 20); // Случайное число в указанном диапазоне
-	echo min(1,4,6)."<br/>"; // Минимально число выводит. Также может найти минимальное в массиве
+	echo "<br/>".md5 ("123");//Кодирует пароль
+	echo "<br/>".trim("   string    sd sd      "); //Пробелы убирают
 	
 ?>
