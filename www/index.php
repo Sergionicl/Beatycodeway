412c71e4e7b35a9e39a48993f91cd0f34cad201f
Larkov Sergey <lavrinzhuk@mail.ru>
2016-01-16 20:46:30 +0300

    COOKIE

--- www/index.php
+++ www/index.php
@@ -1,13 +1,9 @@
 <?php
-
-	// Отрпавка сообщений. PHP не отравляет. Он всего лишь передает информацию приложению sandmail
-	$message = "This is the simlist message in the worrls";
-	$to = "lavrinzhuk@mail.ru";
-	$from = "ok2002@mail.ru";
-	$subject = "Theme of suject";
-	$subject = "=?utf-8?B?".base64_encode($subject)."?=";
-	$headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; 
-	charset = utf-8 \r\n"; // Важно иначе сообщение не будет корректно обрабатываться. Можно указать не plain, a html От кого направлено и кому можно ответить
+	//setcookie("num", 10, time()+30);
+	echo $_COOKIE["num"]."<br/>";
 	
-	mail ($to, $subject, $message, $headers);
+	if (isset ($_COOKIE["num"]))
+		echo "COKI is";
+	else
+		echo "COOKIE isn't";
 ?>
