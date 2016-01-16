ba6135f4f5401d328d81e3b49430a5d8426f793a
Larkov Sergey <lavrinzhuk@mail.ru>
2016-01-16 16:53:57 +0300

    Send email

--- www/index.php
+++ www/index.php
@@ -1,8 +1,13 @@
 <?php
-	//Создание редиректа
-	header("Location: http://google.com"); //не только делает редирект
-	exit; //Вышло и не будет выполняться ниже код
+
+	// Отрпавка сообщений. PHP не отравляет. Он всего лишь передает информацию приложению sandmail
+	$message = "This is the simlist message in the worrls";
+	$to = "lavrinzhuk@mail.ru";
+	$from = "ok2002@mail.ru";
+	$subject = "Theme of suject";
+	$subject = "=?utf-8?B?".base64_encode($subject)."?=";
+	$headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; 
+	charset = utf-8 \r\n"; // Важно иначе сообщение не будет корректно обрабатываться. Можно указать не plain, a html От кого направлено и кому можно ответить
 	
-	$abc = 67;
-	echo $abc++;
+	mail ($to, $subject, $message, $headers);
 ?>
