645863ffb6c86c8181add7f99068189eeab01ac7
Larkov Sergey <lavrinzhuk@mail.ru>
2016-01-16 04:49:27 +0300

    Array finction

--- www/index.php
+++ www/index.php
@@ -1,24 +1,44 @@
 <?php
-   //Строковые переменные
-   $string = "This is example";
+
+	//Функции для массивов
+   $array = array (12, 17, 5, 23, 56);
+   echo count($array)."<br/>";
+   sort ($array); 
+   print_r($array); //.Функция для разработчиков
+   echo "<br/>";
+   rsort ($array);
+   print_r($array); 
+   echo "<br/>";
    
-   echo strlen ($string)."<br/>";
-   echo strpos ($string, "exa")."<br/>"; //Указать позицию данного выражения в строке
-   echo strpos ($string, "is", 4)."<br/>";
-   
-   if (strpos ($string, "T") === false) echo "T is in"; //Необходимо использовать оператор эквивалентности
-   else echo "T isn't";
-   
-   echo "<br/>".substr($string, 3, 7); //Удалил до 3 символа и после 7. Если поставит -2 - то удалит с конца два элемента. Функция гораздо больше 
-   echo str_replace("is", "abc", $string);//Заменяет. Можно массивами str_replace (array("is",))
-   
-   $str = "<b>XXAA, main font<b/>";
-   
-   echo "<br/>". htmlspecialchars ($str);//Добавление спец символов безопасных подменяющих HTML
-   
-   echo "<br/>".strtolower ($string)."<br/>".strtoupper($string);//Верхний и нижний регистры
+   asort ($array); // asort () сортирует значения массивов и привязывает ключи
+   print_r($array); 
+   echo "<br/>";
+   arsort ($array);
+   print_r($array); 
+   echo "<br/>";
 	
-	echo "<br/>".md5 ("123");//Кодирует пароль
-	echo "<br/>".trim("   string    sd sd      "); //Пробелы убирают
-	
+	ksort ($array); // ksort () сортирует значения массивов по ключам
+   print_r($array); 
+   echo "<br/>";
+   krsort ($array);
+   print_r($array); 
+   echo "<br/>";
+   
+   shuffle ($array);//Перемешивает элементы массива
+   print_r ($array);
+   echo "<br/>";
+   
+   echo in_array(23, $array); // Ищет в массиве элемент. Если существует вывводит true или 1
+   echo "<br/>";
+   
+   $arr_1 = array (12, 123, 64);
+   $arr_2 = array (32, 43, 435);
+   
+   $arr_3 = array_merge ($arr_1, $arr_2); //Объединение в один массив
+   print_r ($arr_3);
+   echo "<br/>";
+   
+   $arrDone = array (1, 2, 3, 4, 5, 6, 7, 8,);
+  $arrDone = array_slice ($arrDone, 1, -2); // Аналог strsub(), только с массивом. Здесь первый элемент откидываем и с конца два
+   print_r ($arrDone);
 ?>
