9ace21b305a72e7ed744005c74105609fa95146c
lavrinzhuk <lavrinzhuk@mail.ru>
2016-02-28 09:46:27 +0300

    Saved query with param

--- www/index.php
+++ www/index.php
@@ -1,32 +1,19 @@
 <?php
-	function printResult ($result_set){
-		echo "The number of zapisey -".$result_set-> num_rows."<br/>";
-		while (($row = $result_set-> fetch_assoc())!=false){
-			print_r ($row); //Можно только логин echo ($row["login"])
-			echo "<br/>";
-		}//Вывод записей
+	$mysqli = @new mysqli("localhost", "root", "", "books");
+	if (mysqli_connect_errno()){
+		die(mysqli_connect_error());
 	}
+	$mysqli -> query ('SET NAMES "utf8"');
 	
-	$mysqli = new mysqli("localhost", "root", "", "mybase");
-	$mysqli-> query ("SET NAMES 'utf8'");//Установить кодировку utf8. Очень важно
+	//Подготовленные запросы более безопасные
 	
-	
-	for ($i=1; $i<10; $i++){
-	$mysqli-> query ("INSERT INTO `users` (`login`, `password`, `reg_date`)
-	VALUES ('user_$i', '".md5($i)."', '".time()."' )");
-	}//Добавляем пользователей для работы
-
-	$result_set = $mysqli-> query ("SELECT `userid`, `login` FROM `users` WHERE `userid` >2 ORDER BY `login` ASC LIMIT 0 , 2" ); // Выделяем нужные элементы по критериям, 
-	//упорядочиваем по возсрастанию (ASC, DESC - по убыванию), ограничивает LIMIT 0-с какой начать
-	printResult ($result_set);
-	
-	$result_set = $mysqli -> query ("SELECT `login` FROM `users` WHERE `login` LIKE '%se%' ");//Использование LIKE, чтобы вывести все логины, где встречается "se"
-	printResult ($result_set);
-	
-	$result_set = $mysqli -> query ("SELECT COUNT (`login`) FROM `users` ");//Использование LIKE, чтобы вывести все логины, где встречается "se"
-	printResult ($result_set);
-	
-	
-	$mysqli->close ();//Закрываем соединение, чтобы не накопилось количество соединений превышающее допустимое
-	hello new branchoook
+	$sql = "SELECT  `name` FROM  `customers` WHERE  `customerid` =?";
+	$stmt = $mysqli->prepare($sql);
+	$customerid = 3;
+	// указываем параметр запроса
+	$stmt->bind_param('i', $customerid);
+	$stmt->execute();
+	// привязка переменных к подготовленному запросу для размещения результата
+	$stmt->bind_result($name);
+	while($stmt->fetch()){ echo $name.'<br />';}
 ?>
