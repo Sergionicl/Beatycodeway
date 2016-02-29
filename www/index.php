f7baf2d77183450694cf505b7a1f606623fc249f
lavrinzhuk <lavrinzhuk@mail.ru>
2016-02-29 15:50:41 +0300

    Saved query with param2

--- www/index.php
+++ www/index.php
@@ -1,11 +1,11 @@
 <?php
-	$mysqli = @new mysqli("localhost", "root", "", "books");
+	/* $mysqli = @new mysqli("localhost", "root", "", "books");
 	if (mysqli_connect_errno()){
 		die(mysqli_connect_error());
 	}
 	$mysqli -> query ('SET NAMES "utf8"');
 	
-	//Подготовленные запросы более безопасные
+	//Подготовленные запросы более безопасны
 	
 	$sql = "SELECT  `name` FROM  `customers` WHERE  `customerid` =?";
 	$stmt = $mysqli->prepare($sql);
@@ -16,4 +16,27 @@
 	// привязка переменных к подготовленному запросу для размещения результата
 	$stmt->bind_result($name);
 	while($stmt->fetch()){ echo $name.'<br />';}
+	$mysqli->close();
+	 * 
+	 */	
+
+	$mysqli = @new mysqli("localhost", "root", "", "books");
+	if (mysqli_connect_errno()){
+		die(mysqli_connect_error());
+	}
+	$mysqli -> query ('SET NAMES "utf8"');
+	
+	$sql = "INSERT INTO `customers`(`customerid`, `name`, `address`, `city`) 
+		VALUE (?, ?, ?, ?)";
+	$stmt = $mysqli->prepare($sql);
+	$customerid = "";
+	$name = "Putin";
+	$address = "Zaraevo";
+	$city = "Moscow";
+	
+	$stmt -> bind_param ('isss', $customerid, $name, $address, $city);
+	$stmt -> execute();
+	echo "It is ok";
+	
+	$mysqli->close();
 ?>
