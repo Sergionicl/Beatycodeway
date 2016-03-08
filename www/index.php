d35e3157d59494a54dff5f5d578a9a7bb226c436
lavrinzhuk <lavrinzhuk@mail.ru>
2016-03-08 15:47:27 +0300

    MySQL select, multiple select, group_concat

--- www/index.php
+++ www/index.php
@@ -1,42 +1,43 @@
 <?php
-	/* $mysqli = @new mysqli("localhost", "root", "", "books");
-	if (mysqli_connect_errno()){
-		die(mysqli_connect_error());
-	}
-	$mysqli -> query ('SET NAMES "utf8"');
-	
-	//Подготовленные запросы более безопасны
-	
-	$sql = "SELECT  `name` FROM  `customers` WHERE  `customerid` =?";
-	$stmt = $mysqli->prepare($sql);
-	$customerid = 3;
-	// указываем параметр запроса
-	$stmt->bind_param('i', $customerid);
-	$stmt->execute();
-	// привязка переменных к подготовленному запросу для размещения результата
-	$stmt->bind_result($name);
-	while($stmt->fetch()){ echo $name.'<br />';}
-	$mysqli->close();
-	 * 
-	 */	
 
-	$mysqli = @new mysqli("localhost", "root", "", "books");
-	if (mysqli_connect_errno()){
-		die(mysqli_connect_error());
-	}
-	$mysqli -> query ('SET NAMES "utf8"');
-	
-	$sql = "INSERT INTO `customers`(`customerid`, `name`, `address`, `city`) 
-		VALUE (?, ?, ?, ?)";
-	$stmt = $mysqli->prepare($sql);
-	$customerid = "";
-	$name = "Putin";
-	$address = "Zaraevo";
-	$city = "Moscow";
-	
-	$stmt -> bind_param ('isss', $customerid, $name, $address, $city);
-	$stmt -> execute();
-	echo "It is ok";
-	
-	$mysqli->close();
+//Multiple Select 
+
+$sql = "SELECT orders.orderid, orders.amount, orders.date
+FROM customers, orders
+WHERE customers.name = 'Putin'
+AND customers.customerid = orders.customerid";
+
+//Finding Rows That join 
+$sql = "SELECT customers.customerid, customers.name, orders.orderid
+FROM customers 
+LEFT JOIN orders
+ON customers.customerid = orders.customerid;
+";
+
+// Группировка
+Данные в табличке вот такие
+маша - попугай
+света - кошка
+маша - хомяк
+юля - собака
+света - собака
+маша - рыбки
+
+Из этой таблички видно, что
+у Маши есть попугай, хомяк и рыбки,
+у Светы кошка и собака,
+а у Юли только собака.
+Так вот есть задача вывести всех людей из таблицы и чтобы возле каждого показывалось, какие животные у него есть.
+
+Делаем вот такой запрос
+SELECT `name`, GROUP_CONCAT(`pet`) as `pet`
+FROM `test`
+GROUP BY `name`
+
+Результат получается такой
+маша - попугай,хомяк,рыбки
+света- кошка,собака
+юля - собака 
+
+
 ?>
